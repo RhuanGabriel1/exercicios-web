83271559d6467e023d50a1fd69f9077cc696c5c8
Rhuan Martins <rhuan.gabriel2001@icloud.com>
2022-08-05 17:09:44 -0300

    criando um dropdown list da cidade baseado no estado selecionado

--- aula01/assets/script.js
+++ aula01/assets/script.js
@@ -7,21 +7,7 @@ const  inputRua  = document.getElementById("rua");
 const  inputNascimento  = document.getElementById("nascimento");
 const  inputNome  = document.getElementById("nome");
 const  inputSubmit  = document.getElementById("enviar");
-
-const buscarEstados = (e) =>{
-    const url = "https://servicodados.ibge.gov.br/api/v1/localidades/estados?orderBy=nome";
-    fetch(url).then(response => response.json())
-        .then((json) =>{
-            console.log(json);
-            for(let i=0; i < json.length; i++){
-                const option = document.createElement("option");
-                option.value = json[i].id;
-                const texto = document.createTextNode(json[i].nome);
-                option.appendChild(texto);
-                selectEstado.appendChild(option);
-            }
-        });
-}
+let idAux;
 
 const buscarPaises = (e) =>{
     const url = "https://servicodados.ibge.gov.br/api/v1/localidades/paises";
@@ -35,14 +21,42 @@ const buscarPaises = (e) =>{
                 option.appendChild(texto);
                 selectPais.appendChild(option);
             }
+
         });
 }
+const buscarEstados  = async (e) =>{
+    const url = "https://servicodados.ibge.gov.br/api/v1/localidades/estados?orderBy=nome";
+    await fetch(url).then(response => response.json())
+        .then((json) =>{
+            console.log(json);
+            for(let i=0 ;i < json.length; i++){
+                const option = document.createElement("option");
+                option.value = json[i].id;
+                const texto = document.createTextNode(json[i].nome);
+                option.appendChild(texto);
+                selectEstado.appendChild(option);
+            }
+            selectEstado.addEventListener("change", e => buscarCidades(selectEstado.value));
+        });
+    }
 
 const buscarCidades = (e) =>{
-    const url = "https://servicodados.ibge.gov.br/api/v1/localidades/estados/33/municipios?orderBy=nome"
-    
+    const url = "https://servicodados.ibge.gov.br/api/v1/localidades/estados/"+e+"/municipios?orderBy=nome"
+    console.log(url);
+    fetch(url).then(response => response.json())
+        .then((json) =>{
+            console.log(json);
+            for(let i=0; i < json.length; i++){
+                const option = document.createElement("option");
+                option.value = json[i].id;
+                const texto = document.createTextNode(json[i].nome);
+                option.appendChild(texto);
+                selectCidade.appendChild(option);
+            }
+        });
+
 }
 
-buscarEstados();
 buscarPaises();
+buscarEstados();
 buscarCidades();
