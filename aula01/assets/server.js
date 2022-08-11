21564ac5a8260245ae8cf92a6fad526e93faf157
Rhuan Martins <rhuan.gabriel2001@icloud.com>
2022-08-11 20:14:36 -0300

    novo método utilizando replace

--- aula01/assets/server.js
+++ aula01/assets/server.js
@@ -4,13 +4,19 @@ let port = 3000;
 let server = http.createServer(function(req, res){
     let pagina = "";
     let rota = req.url;
-    let urlNoBackSlash = rota.split('/?&');
+    let urlNoBackSlash = rota.replace("/?", "");
+    urlNoBackSlash = urlNoBackSlash.replace("/favicon", "");
+    urlNoBackSlash = urlNoBackSlash.replace(".ico", "");
+    urlNoBackSlash = urlNoBackSlash.replace("+", " ");
+    urlNoBackSlash = urlNoBackSlash.replace("&", ", ");
     console.log(urlNoBackSlash);
-    // let urlNoQuestionPoint = urlNoBackSlash.;
-    // console.log(urlNoQuestionPoint);
-
     res.end(pagina);
+    getText();
 });
+
+function getText (){
+    console.log(urlNoBackSlash);;
+} 
 
 server.listen(port);
 console.log("Servidor escutando na porta: ", port);
