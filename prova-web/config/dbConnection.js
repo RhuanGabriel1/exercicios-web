aabb80d228719464ab5d42187cac340faedab5c2
Rhuan Martins <rhuan.gabriel2001@icloud.com>
2022-12-11 04:30:37 -0300

    testes

--- prova-web/config/dbConnection.js
+++ prova-web/config/dbConnection.js
@@ -1,7 +1,7 @@
 const {MongoClient} = require('mongodb');
 require('dotenv').config();
 
-console.log("DOTENV -> "+process.env.mongoUri);
+// console.log("DOTENV -> "+process.env.mongoUri);
 
 const client = new MongoClient(process.env.mongoUri);
 
